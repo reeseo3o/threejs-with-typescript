36949cac87a895a3ced55253b17fdce874889fac
Reese (Yaeji) <notyaeji@gmail.com>
2024-11-10 19:32:37 +0900

    Feat: 03-material 폴더 추가

---
+++ 03-material/src/main.ts
@@ -0,0 +1,95 @@
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+import "./style.css";
+import * as THREE from "three";
+import GUI from "three/examples/jsm/libs/lil-gui.module.min.js";
+
+class App {
+  private domApp: Element;
+  private renderer: THREE.WebGLRenderer;
+  private scene: THREE.Scene;
+  private camera?: THREE.PerspectiveCamera;
+
+  constructor() {
+    this.domApp = document.querySelector("#app")!;
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.domApp.appendChild(this.renderer.domElement);
+    this.scene = new THREE.Scene();
+
+    this.setupCamera();
+    this.setupLight();
+    this.setupModels();
+    this.setupEvents();
+  }
+
+  private setupCamera() {
+    const domApp = this.domApp;
+    const width = domApp.clientWidth;
+    const height = domApp.clientHeight;
+
+    this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
+    this.camera.position.z = 2;
+
+    new OrbitControls(this.camera, this.domApp as HTMLElement);
+  }
+
+  private setupLight() {
+    const color = 0xffffff;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    this.scene.add(light);
+  }
+
+  private setupModels() {
+    const geometry = new THREE.SphereGeometry();
+    geometry.deleteAttribute("uv");
+    // SphereGeometry에 할당된 uv attribute때문임. PointsMaterial의 uv 속성 제거
+    const circle = new THREE.TextureLoader().load("./circle.png");
+    const material = new THREE.PointsMaterial({
+      color: 0xff000,
+      size: 5,
+      sizeAttenuation: false,
+      map: circle,
+      alphaTest: 0.5,
+      // 이미지 픽셀 값중 알파박ㅂㅅ이 0.5보다 클 경우만 이미지 이용하게 하는 속성
+    });
+    const points = new THREE.Points(geometry, material);
+    this.scene.add(points);
+
+    const gui = new GUI();
+    gui.add(material, "size", 0.1, 10, 0.01);
+  }
+
+  private setupEvents() {
+    window.onresize = this.resize.bind(this);
+    this.resize();
+    this.renderer.setAnimationLoop(this.render.bind(this));
+  }
+
+  private resize() {
+    const domApp = this.domApp;
+    const width = domApp.clientWidth;
+    const height = domApp.clientHeight;
+
+    const camera = this.camera;
+    if (camera) {
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+    }
+
+    this.renderer.setSize(width, height);
+  }
+
+  private update(time: number) {
+    time *= 0.001; // ms -> s
+  }
+
+  private render(time: number) {
+    this.update(time);
+    this.renderer.render(this.scene, this.camera!);
+  }
+}
+
+new App();
