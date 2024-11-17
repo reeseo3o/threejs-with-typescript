ba6b35ae4d2de1e046f0f89a3736782afc1fec50
Reese (Yaeji) <notyaeji@gmail.com>
2024-11-18 00:26:53 +0900

    Feat: LineDashedMaterial 적용

--- 03-material/src/main.ts
+++ 03-material/src/main.ts
@@ -52,8 +52,16 @@ class App {
     // Float32BufferAttribute 객체는 하나의 값이 32비트 실수 데이터가 저장된 버퍼 객체
     // 3은 배열에서 3개씩 묶어서 하나의 값(좌표)으로 취급하겠다는 의미
 
-    const material = new THREE.LineBasicMaterial({ color: 0x00ff00 });
+    const material = new THREE.LineDashedMaterial({
+      color: 0x00ff00,
+      dashSize: 0.2,
+      gapSize: 0.1,
+      scale: 1,
+      // scale - 라인 전체의 표현 횟수 지정
+    });
+    // 재질 적용 시 각 라인의 길이를 알고 있어야 함으로 각 라인의 길이 계산 필요
     const line = new THREE.Line(geometry, material);
+    line.computeLineDistances();
     this.scene.add(line);
   }
 
